b3829a13f7830d4e348b55267547300699b988de
Denis <denissolovyovv@gmail.com>
2024-11-08 04:02:33 +0300

    WithOOP

--- SopromatSterCpp.cpp
+++ SopromatSterCpp.cpp
@@ -1,194 +1,178 @@
 ﻿/*
     Программа предназначена решать задачу на растяжение-сжатие по сопромату.
-    Это первый черновой вариант, дальше планируется его улучшение
+    Это черновой вариант, дальше планируется его улучшение
     - выведение некоторых частей кода в функции, создание клаассов и графической части.
 
     Слудующие шаги:
     (сделано) - добавить вторую заделку
     - добавить распределённую нагрузку, в том числе и в виде веса
-    - создать функция для вывода результатов, вынести повторяющийся код
-    - создать классы для решения отдельных задач
+    (сделано) создать функция для вывода результатов, вынести повторяющийся код
+    (в процессе) создать классы для решения отдельных задач
+
+
+    -проверить displacements
+    -перевести единицы измерения
 */
 
+
+
 #include <iostream>
-#include <cmath>
+#include <vector>
 
-//                const vector<double>& innerForces
-void printResults(double* innerForces, double* sigmas, double* deltaLens, double* displacements, int n, double sigmaMax, double sigmaT, double A, double U)
-{
-    std::cout << "=====================================================================\n\n";
-    std::cout << "Вывод результатов при движении от свободного конца слева" << std::endl;
-    std::cout << "\n\n";
-        
-    std::cout << "Значения внутренних нормальных сил [кН]: " << std::endl;
-    for (int i = 0; i < n; i++)
-    {
-        std::cout << innerForces[i] << ",\t";
+class Rod {
+public:
+    Rod(int n, double modulE, int fixersNumber)
+        : n(n), modulE(modulE), fixersNumber(fixersNumber) {
+        sections.resize(n);
     }
-    std::cout << "\n\n";
 
-    std::cout << "Значения нормальных напряжений [МПа]: " << std::endl;
-    for (int i = 0; i < n; i++)
-    {
-        std::cout << sigmas[i] << ",\t";
+    void setSection(int index, double area, double length, double force) {
+        if (index >= 0 && index < n) {
+            sections[index] = Section(area, length, force);
+        }
+        else {
+            std::cout << "Ошибка: индекс участка вне диапазона." << std::endl;
+        }
     }
-    std::cout << "\n\n";
 
-    std::cout << "Значения удлинений участков [10^(-4) м]: " << std::endl;
-    for (int i = 0; i < n; i++)
-    {
-        std::cout << deltaLens[i] << ",\t";
+    void calculateInnerForces() {
+        double previousInnerForce = 0;
+
+        for (size_t i = 0; i < sections.size(); ++i) {
+            sections[i].innerForce = previousInnerForce + sections[i].force;
+            previousInnerForce = sections[i].innerForce;
+        }
     }
-    std::cout << "\n\n";
 
-    std::cout << "Значения перемещений поперечных сечений участков [10^(-4) м]: " << std::endl;
-    for (int i = 0; i < n + 1; i++)
-    {
-        std::cout << displacements[i] << ",\t";
+    void calculateStress() {
+        for (size_t i = 0; i < sections.size(); ++i) {
+            sections[i].stress =  sections[i].innerForce * 10 / sections[i].area;  // Перевод в МПа
+        }
     }
-    std::cout << "\n\n\n";
 
-    std::cout << "Коэффициент запаса прочности по текучести n=" << sigmaMax / sigmaT;
-    std::cout << "\n\n";
+    void calculateDeformation() {
+        for (size_t i = 0; i < sections.size(); ++i) {
+            sections[i].deformation = sections[i].innerForce * sections[i].length * 100000 / (modulE * sections[i].area);
+        }
+    }
 
-    std::cout << "Работа внешних сил A = " << A;
-    std::cout << "\n\n";
+    void calculateDisplacement() {
+        for (size_t i = sections.size() - 1; i > 0; --i) {
+            nodes[i].displacement = nodes[i+1].displacement + sections[i - 1].deformation;
+        }
+    }
 
-    std::cout << "Энергия деформирования U = " << U;
-    std::cout << "\n\n";
-}
+    // Тут должна быть обработка случая с двумя заделками
 
-int main()
-{
+    void calculateWorkAndEnergy() {
+        for (int i = 0; i < n; i++) {
+            A += 0.5 * sections[i].force * nodes[i].displacement;
+            U += 0.5 * sections[i].innerForce * sections[i].deformation;
+        }
+
+        A /= 10;   // Перевод в Дж
+        U /= 10;   // Перевод в Дж
+    }
+
+    void printInfo() const {
+        std::cout << "Количество участков стержня: " << sections.size() << "\n\n";
+        std::cout << "Модуль упругости: " << modulE << " МПа" << "\n\n";
+        std::cout << "Количество заделок по концам: " << fixersNumber << "\n\n";
+
+        for (size_t i = 0; i < sections.size(); ++i) {
+            std::cout << "Участок " << (i + 1) << ":" << "\n\n";
+            std::cout << "  Длина: " << sections[i].length << " м" << "\n\n";
+            std::cout << "  Площадь: " << sections[i].area << " см^2" << "\n\n";
+            std::cout << "  Внешняя сила: " << sections[i].force << " кН" << "\n\n";
+            std::cout << "  Внутренняя сила: " << sections[i].innerForce << " кН" << "\n\n";
+            std::cout << "  Внутреннее напряжение: " << sections[i].stress << " МПа" << "\n\n";
+        }
+
+        std::cout << "Смещения узлов:" << "\n\n";
+        for (size_t i = 0; i < nodes.size(); ++i) {
+            std::cout << "  Узел " << (i + 1) << ": Смещение = " << nodes[i].displacement << " м" << "\n\n";
+        }
+
+        std::cout << "Работа внешних сил A = " << A;
+        std::cout << "\n\n";
+
+        std::cout << "Энергия деформирования U = " << U;
+        std::cout << "\n\n";
+    }
+
+
+    class Section {
+    public:
+        double area;       // Площадь поперечного сечения
+        double length;     // Длина участка
+        double force;      // Внешняя сосредоточенная сила в начале участка
+        double innerForce; // Внутренняя сила
+        double stress;     // Внутреннее нормальное напряжение
+        double deformation;// Изменение длины
+
+        Section(double area = 0, double length = 0, double force = 0)
+            : area(area), length(length), force(force), innerForce(0), stress(0), deformation(0) {}
+    };
+
+    class Node {
+    public:
+        double displacement; // Продольное перемещение поперечного сечения
+
+        Node(double displacement = 0) : displacement(displacement) {}
+    };
+
+private:
+    int n;                         // Количество участков стержня
+    double modulE;                 // Модуль упругости
+    int fixersNumber;              // Количество заделок по концам стержня
+    double A = 0.0;                // Работа внешних сил
+    double U = 0.0;                // Потенциальная энергия деформирования
+    std::vector<Section> sections; // Вектор для хранения участков
+    std::vector<Node> nodes;       // Вектор узлов
+};
+
+int main() {
     setlocale(LC_ALL, "ru");
-    int n; // Количество участков стержня
-    double modulE; // Модуль упругости
-    int fixersNumber = 0; // Количество заделок по концам стержня
-    double sigmaMax = 0;
-    double sigmaT;
-    double A = 0;
-    double U = 0;
-    //int resultParts; 
-    //resultParts = 2;
-    //const double Pi = 3.14159;
 
-    while ((fixersNumber != 1) && (fixersNumber != 2))
-    {
-        std::cout << "Введите количество заделок стержня (1 или 2) :";
-        std::cin >> fixersNumber;
-    }
+    int n;
+    double modulE;
 
-    std::cout << "Введите модуль упругости E [МПа] :";
-    std::cin >> modulE;
-    std::cout << "Введите предел прочности [МПа] :";
-    std::cin >> sigmaT;
-    std::cout << "Введите количество участвок стержня: ";
+    // Ввод данных
+    std::cout << "Введите количество участков стержня: ";
     std::cin >> n;
 
-    double* lengths = new double[n]; // vector<double> lengths(n);
-    double* areas = new double[n];
-    double* forces = new double[n];
-    double* innerForces = new double[n];
-    double* sigmas = new double[n];
-    double* deltaLens = new double[n];
-    double* displacements = new double[n + 1] {};// Перемещения поперечных сечений в узлах(концах участков) стержня
+    std::cout << "Введите модуль упругости (МПа): ";
+    std::cin >> modulE;
 
-    /*for (int i = 0; i <= n; i++)
-    {
-        displacements[i] = 0.0;
-    }*/
+    int fixersNumber;
+    do {
+        std::cout << "Введите количество заделок (1 или 2): ";
+        std::cin >> fixersNumber;
 
-    // Ввод исходных данных
-    for (int i = 0; i < n; i++)
-    {
-        std::cout << "Введите длину участка " << i + 1 << " [м] : ";
-        std::cin >> lengths[i];
-
-        std::cout << "Введите площадь поперечного сечения на участке " << i + 1 << " [см^2] : ";
-        std::cin >> areas[i];
-
-        std::cout << "Введите внушнюю силу в начале участка " << i + 1 << " [кН] : ";
-        std::cin >> forces[i];
-        innerForces[i] = forces[i];
-    }
-
-    // Определение внутренних нормальных сил, нормальных напряжений и удлинений участков
-    for (int i = 0; i < n; i++)
-    {
-        if (i != 0)
-        {
-            innerForces[i] += innerForces[i - 1];
+        if (fixersNumber != 1 && fixersNumber != 2) {
+            std::cout << "Ошибка: необходимо ввести 1 или 2." << std::endl;
         }
+    } while (fixersNumber != 1 && fixersNumber != 2);
 
-        sigmas[i] = innerForces[i] / areas[i] * 10;
-        deltaLens[i] = innerForces[i] * lengths[i] * 100000 / (modulE * areas[i]);
+    Rod rod(n, modulE, fixersNumber);
+
+    for (int i = 0; i < n; ++i) {
+        double length, area, force;
+        std::cout << "Участок " << (i + 1) << ":" << std::endl;
+        std::cout << "Введите длину (м): ";
+        std::cin >> length;
+        std::cout << "Введите площадь поперечного сечения (см^2): ";
+        std::cin >> area;
+        std::cout << "Введите величину внешней силы (кН): ";
+        std::cin >> force;
+
+        rod.setSection(i, area, length, force);
     }
 
-    // Определение перемещений поперечных сеений
-    for (int i = n - 1; i >= 0; i--)
-    {
-        displacements[i] = displacements[i + 1] + deltaLens[i];
-    }
+    rod.calculateInnerForces();
+    rod.calculateStress();
+    rod.calculateDeformation();
+    rod.printInfo();
 
-    // Определение Fx
-    // 1. Аналитически
-    double dispTotal = displacements[0];
-    //std::cout << " dispTotal = " << dispTotal;
-    double dispFx = 0;
-    for (int i = 0; i < n; i++)
-    {
-        dispFx += lengths[i] * 100000 / (modulE * areas[i]);
-    }
-    //std::cout << " dispFx = " << dispFx;
-    double Fx = -dispTotal / dispFx;
-    // 2. Перевобом
-
-    std::cout << " Fx = " << Fx;
-
-
-    if (fixersNumber == 2)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            innerForces[i] += Fx;
-            sigmas[i] = innerForces[i] / areas[i] * 10;
-            deltaLens[i] = innerForces[i] * lengths[i] * 100000 / (modulE * areas[i]);
-        }
-        for (int i = n - 1; i >= 0; i--)
-        {
-            displacements[i] = displacements[i + 1] + deltaLens[i];
-        }
-    }
-
-    // Определение максимального напряжения
-    for (int i = 0; i < n; i++)
-    {
-        if (fabs(sigmas[i]) > sigmaMax)
-            sigmaMax = fabs(sigmas[i]);
-    }
-
-    //Определение работы внешних сил
-    for (int i = 0; i < n; i++)
-    {
-        A += 0.5 * forces[i] * displacements[i];
-    }
-    A /= 10;
-
-    //Определение потенциальной энергии деформаций
-    for (int i = 0; i < n; i++)
-    {
-        U += 0.5 * innerForces[i] * deltaLens[i];
-    }
-    U /= 10;
-
-    // Вывод результатов
-    printResults(innerForces, sigmas, deltaLens, displacements, n, sigmaMax, sigmaT, A, U);
-
-    delete[] displacements;
-    delete[] deltaLens;
-    delete[] sigmas;
-    delete[] innerForces;
-    delete[] forces;
-    delete[] areas;
-    delete[] lengths;
+    return 0;
 }
